ebb82f89a3eb8cdabeea19b79514528b8b35e08f
HAARP <70611773+H-Art-Src@users.noreply.github.com>
2023-11-04 15:09:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+#Author: James Raw
+
 import ctypes as c
-from ctypes import CDLL, Structure, POINTER
+from ctypes import CDLL, Structure
 import discogs_client
 import urllib.request 
 import os
 from PIL import Image
 
-uiFunc = CDLL("build/ui.so")
+
+#--Query Discogs for release data.
 d = discogs_client.Client('my_user_agent/1.0', user_token='ithHqUJMHmgldAjtELqHEzMyzQyxJDeaIdLpoLsp')
-
-class albumEntry(Structure):
-    _fields_ = [('title', c.c_char_p),
-                ('desc', c.c_char_p),
-                ('img', c.c_char_p)]
-
+#test list
 enteredList = [133048 , 20017387 , 1010101010101 , 451 , 163867 , 1337]
 finalList = []
 for releaseNum in enteredList:
@@ -25,24 +23,40 @@ for releaseNum in enteredList:
     else:
         finalList.append(testR)
 
+
+#--ctypes struct for raylib c struct, must match the exact variable names and types of the struct declared in c file.
+class albumEntry(Structure):
+    _fields_ = [('title', c.c_char_p),
+                ('desc', c.c_char_p),
+                ('img', c.c_char_p)]
+#--prepare passable array of ctypes struct.
+print("\n Preparing array \n")
 entryArr = (albumEntry * len(finalList))()
 i = 0
 for release in finalList:
-    print("final listing - " + release.title)
     path = "coverImages/" + str(release.id) + ".jpeg"
     path2 = "coverImages/" + str(release.id) + ".png"
     if not os.path.isfile(path2):
         urllib.request.urlretrieve(release.images[0]['uri'], path)
+        #raylib does not support jpg, convert to png.
         im = Image.open(path)
         im.save(path2)
         os.remove(path)
-    print(release.images[0]['uri'])
-    entryArr.__setitem__(i , (albumEntry( release.title.encode("ascii") , release.artists[0].name.encode("ascii")  , path2.encode("ascii") )))
+
+    #raylib struct: titles, description body (for now just first artist), image/album cover path
+    entryArr.__setitem__(i , (albumEntry( release.title.encode("ascii") , release.artists[0].name.encode("ascii")  , path2.encode("ascii") ) ))
+    for field_name, field_type in entryArr[i]._fields_:
+        print (field_name, getattr(entryArr[i], field_name))
+    print("~")
     i += 1
 
-@c.CFUNCTYPE(None)
-def callback():
-    print("Yes!!!!")
-uiFunc.justgo(callback , entryArr , len(finalList))
-
-print("end python main")
+#--Pass the array of structs to c library.
+try:
+    uiFunc = CDLL("build/ui.so")
+    @c.CFUNCTYPE(None)
+    def callback():
+        print("test callback")
+    #call raylib loop
+    uiFunc.justgo(callback , entryArr , len(finalList))
+except:
+    print("No ui.so file found or error.")
