80ed3adf3a3ddfed87ee1c4d2b481290c8458c5a
HAARP <awwdsm@gmail.com>
2023-11-07 23:19:43 -0500

    cc command has no depency on raylib

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cc -fPIC -shared -o build/ui.so src/ui.c -lraylib
+cc -fPIC -shared -o build/ui.so src/ui.c -s -static -Os -std=c99 -Wall -Ivendor/raylib/src -Iexternal -DPLATFORM_DESKTOP -lraylib -lopengl32 -lgdi32 -lwinmm
 
 if [ $? -ne 0 ] 
 then
